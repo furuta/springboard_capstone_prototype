72ac9c5e0f93f06cf0166139b24e58cc41b14add
furuta <>
2019-06-01 12:28:30 -0400

    [add] get neighborhood info from google api

--- projects/price_prediction/main.py
+++ projects/price_prediction/main.py
@@ -5,12 +5,16 @@ import luigi
 import pickle
 import datetime
 import locale
+import requests
+import json
 
 class DataPreparingTask(luigi.Task):
     calendar_csv_filename = luigi.Parameter()
     listings_csv_filename = luigi.Parameter()
     using_cols = luigi.Parameter()
     intermediate_data_path = luigi.Parameter()
+    google_places_api_url = luigi.Parameter()
+    api_key = luigi.Parameter()
 
     def run(self):
         calendar_data_frame = pd.read_csv(self.calendar_csv_filename)
@@ -39,8 +43,6 @@ class DataPreparingTask(luigi.Task):
         calendar_data_frame['day_of_week'] = calendar_data_frame['datetime'].map(lambda x:x.weekday())
         calendar_data_frame = pd.get_dummies(calendar_data_frame, columns=['month', 'day_of_week'])
 
-        # print(calendar_data_frame.loc[0:3,:].to_csv())
-
         ########################
         # pregare listing data #
         ########################
@@ -59,19 +61,32 @@ class DataPreparingTask(luigi.Task):
         listing_data_frame = listing_data_frame.rename(columns={'id': 'listing_id'})
 
         # latitude, longitude
+        # Google Places API
+        radius = '300'
+        language = 'en'
+        for index, row in listing_data_frame.iterrows():
+            response = requests.get(self.google_places_api_url + 
+                        'key=' + self.api_key + 
+                        '&location=' + str(row.latitude) + ',' + str(row.longitude) + 
+                        '&radius=' + radius + 
+                        '&language=' + language)
+            data = response.json()
+            for result in data['results']:
+                if not result['types'][0] in listing_data_frame.columns:
+                    listing_data_frame[result['types'][0]] = 0
+                listing_data_frame.loc[index, result['types'][0]] += 1
 
         # property_type, room_type, cancellation_policy
         listing_data_frame = pd.get_dummies(listing_data_frame, columns=['property_type', 'room_type', 'cancellation_policy'])
-
-        # print(listing_data_frame.loc[0:1,:].to_csv())
 
         ####################
         # marge and output #
         ####################
         intermediate_data_frame = pd.merge(listing_data_frame, calendar_data_frame, on='listing_id')
 
-        with open(self.output().path, "wb") as target:
-            pickle.dump(intermediate_data_frame, target)
+        intermediate_data_frame.to_csv(self.intermediate_data_path, index = None)
+        # with open(self.output().path, "wb") as target:
+        #     pickle.dump(intermediate_data_frame.to_csv(), target)
 
     def output(self):
         return luigi.LocalTarget(self.intermediate_data_path)
