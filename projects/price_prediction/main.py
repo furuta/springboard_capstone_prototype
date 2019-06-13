b2073e6fb63b7282bb221abd13e61b2152daa6b6
furuta <>
2019-06-12 20:22:10 -0400

    [edit] change intermediate file name [edit] change variable name [add] delete unused column

--- projects/price_prediction/main.py
+++ projects/price_prediction/main.py
@@ -17,8 +17,8 @@ class DataPreparingTask(luigi.Task):
     api_key = luigi.Parameter()
 
     def run(self):
-        calendar_data_frame = pd.read_csv(self.calendar_csv_filename)
-        listing_data_frame = pd.read_csv(self.listings_csv_filename)
+        df_calendar = pd.read_csv(self.calendar_csv_filename)
+        df_listing = pd.read_csv(self.listings_csv_filename)
 
         #########################
         # pregare calendar data #
@@ -29,19 +29,22 @@ class DataPreparingTask(luigi.Task):
             # 'available',
             'price',
         ]
-        calendar_data_frame = calendar_data_frame.loc[:, use_columns_in_calendar]
+        df_calendar = df_calendar.loc[0:1000, use_columns_in_calendar]
 
         # price
-        calendar_data_frame['price_amount'] = calendar_data_frame['price'].map(lambda x:float(str(x).replace(',', '').replace('$', '')))
-        calendar_data_frame.loc[calendar_data_frame['price_amount'] > 0, :]
+        df_calendar['price_amount'] = df_calendar['price'].map(lambda x:float(str(x).replace(',', '').replace('$', '')))
+        df_calendar.loc[df_calendar['price_amount'] > 0, :]
         # date
         locale.setlocale(locale.LC_TIME, 'ja_JP')
-        calendar_data_frame['datetime'] = calendar_data_frame['date'].map(lambda x:datetime.datetime.strptime(str(x), '%Y-%m-%d'))
-        # calendar_data_frame['year'] = calendar_data_frame['datetime'].map(lambda x:x.year)
-        calendar_data_frame['month'] = calendar_data_frame['datetime'].map(lambda x:x.month)
-        calendar_data_frame['day'] = calendar_data_frame['datetime'].map(lambda x:x.day)
-        calendar_data_frame['day_of_week'] = calendar_data_frame['datetime'].map(lambda x:x.weekday())
-        calendar_data_frame = pd.get_dummies(calendar_data_frame, columns=['month', 'day_of_week'])
+        df_calendar['datetime'] = df_calendar['date'].map(lambda x:datetime.datetime.strptime(str(x), '%Y-%m-%d'))
+        # df_calendar['year'] = df_calendar['datetime'].map(lambda x:x.year)
+        df_calendar['month'] = df_calendar['datetime'].map(lambda x:x.month)
+        df_calendar['day'] = df_calendar['datetime'].map(lambda x:x.day)
+        df_calendar['day_of_week'] = df_calendar['datetime'].map(lambda x:x.weekday())
+        df_calendar = pd.get_dummies(df_calendar, columns=['month', 'day_of_week'])
+        del df_calendar['date']
+        del df_calendar['price']
+        del df_calendar['datetime']
 
         ########################
         # pregare listing data #
@@ -57,14 +60,14 @@ class DataPreparingTask(luigi.Task):
             'beds',
             'cancellation_policy',
         ]
-        listing_data_frame = listing_data_frame.loc[:, use_columns_in_listing]
-        listing_data_frame = listing_data_frame.rename(columns={'id': 'listing_id'})
+        df_listing = df_listing.loc[:, use_columns_in_listing]
+        df_listing = df_listing.rename(columns={'id': 'listing_id'})
 
         # latitude, longitude
         # Google Places API
         radius = '300'
         language = 'en'
-        for index, row in listing_data_frame.iterrows():
+        for index, row in df_listing.iterrows():
             response = requests.get(self.google_places_api_url + 
                         'key=' + self.api_key + 
                         '&location=' + str(row.latitude) + ',' + str(row.longitude) + 
@@ -72,21 +75,24 @@ class DataPreparingTask(luigi.Task):
                         '&language=' + language)
             data = response.json()
             for result in data['results']:
-                if not result['types'][0] in listing_data_frame.columns:
-                    listing_data_frame[result['types'][0]] = 0
-                listing_data_frame.loc[index, result['types'][0]] += 1
+                if not result['types'][0] in df_listing.columns:
+                    df_listing[result['types'][0]] = 0
+                df_listing.loc[index, result['types'][0]] += 1
 
         # property_type, room_type, cancellation_policy
-        listing_data_frame = pd.get_dummies(listing_data_frame, columns=['property_type', 'room_type', 'cancellation_policy'])
+        df_listing = pd.get_dummies(df_listing, columns=['property_type', 'room_type', 'cancellation_policy'])
+
+        del df_listing['latitude']
+        del df_listing['longitude']
 
         ####################
         # marge and output #
         ####################
-        intermediate_data_frame = pd.merge(listing_data_frame, calendar_data_frame, on='listing_id')
+        df_intermediate = pd.merge(df_listing, df_calendar, on='listing_id')
+        del df_intermediate['listing_id']
 
-        intermediate_data_frame.to_csv(self.intermediate_data_path, index = None)
-        # with open(self.output().path, "wb") as target:
-        #     pickle.dump(intermediate_data_frame.to_csv(), target)
+        with open(self.output().path, "wb") as target:
+            pickle.dump(df_intermediate, target)
 
     def output(self):
         return luigi.LocalTarget(self.intermediate_data_path)
